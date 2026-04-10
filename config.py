117f0d9f8331b9ac3a8175e08d4647d4873781fb
anujedits42-byte <anujedits42@gmail.com>
2026-04-10 10:27:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ from os import environ
 # For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
 class Config:
-    API_ID = environ.get("API_ID", "26847865")
-    API_HASH = environ.get("API_HASH", "0ef9fdd3e5f1ed49d4eb918a07b8e5d6")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7782443871:AAEC3CMt6JU_6-ZIFXSVhrUcwIRf_ztHBuY") 
+    API_ID = environ.get("API_ID", "34446649")
+    API_HASH = environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8760503947:AAHelwW2hXYNEQXZAKVKPPZV-DQMVU8f_mQ") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://1by1themes:3snVjsLPmZ9xcbd3@cluster0.uaazt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "forward")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5597521952').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002140125423'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "jn_bots") 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Anujedit")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7892805795').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003515041061'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "log_channel_a") 
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8081')
     
 #Dont Remove My Credit @Silicon_Bot_Update 
