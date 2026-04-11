eb0efe211588d4ab8986191b83d2e1103b96e107
anujedits42-byte <anujedits42@gmail.com>
2026-04-11 07:07:07 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -11,6 +11,15 @@ from pyrogram.raw.all import layer
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message 
 from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
 from pyrogram.errors import FloodWait
+
+from pyrogram.errors import (
+    PhoneNumberInvalid,
+    PhoneCodeInvalid,
+    PhoneCodeExpired,
+    SessionPasswordNeeded,
+    PasswordHashInvalid
+)
+
 from config import Config
 from translation import Translation
 #Dont Remove My Credit @Silicon_Bot_Update 
