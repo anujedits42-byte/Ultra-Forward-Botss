8e31552664e8b95d1fdf30e1d0c029e362d2464a
anujedits42-byte <anujedits42@gmail.com>
2026-04-11 05:55:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -215,5 +215,5 @@ async def server_status(bot, query):
 @Client.on_message(filters.private & filters.command(['donate']))
 async def restart(client, message):
     msg = await message.reply_text(
-        text="<i>__If you liked my service❤__.\n\nConsider and make a donation to support my developer 👦\n\n\nUPI ID - `pay-to-yash-singh@fam`</i>"
+        text="<i>__If you liked my service❤__.\n\nConsider and make a donation to support my developer 👦\n\n\nUPI ID - `971916880@ybl`</i>"
         )
