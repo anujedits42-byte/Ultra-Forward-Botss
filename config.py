bb9c39bee3dab912716ae2efb4ec0cbd60022316
anujedits42-byte <anujedits42@gmail.com>
2026-04-13 22:09:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "34446649")
     API_HASH = environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "8760503947:AAHelwW2hXYNEQXZAKVKPPZV-DQMVU8f_mQ") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8779354652:AAEKi1dY-JkMz_WVWLSLD3qduSwIQcbyqrU") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Anujedit")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7892805795').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003515041061'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "log_channel_a") 
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003475522251'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "anujeditbyak") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8081')
     
