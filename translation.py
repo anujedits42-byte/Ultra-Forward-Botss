6e40ed83a8b4b62efbf4fdb460d798082ee5fc9f
anujedits42-byte <anujedits42@gmail.com>
2026-04-13 23:26:01 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,7 +7,7 @@ from config import Config
 
 class Translation(object):
 
-  PHOTO_URL = "https://image.zaw-myo.workers.dev/image/c910e030-0067-4855-bd39-4dffb60780ca"
+  PHOTO_URL = ""
   
   START_TXT = """<b>ʜɪ {}
 
